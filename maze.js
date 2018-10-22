3c1ce8cf185db547f8606b14e1823c93f2eb715f
Lindsay Pink <43824268+LindsayPink@users.noreply.github.com>
2018-10-22 17:23:59 -0500

    single boundary turns red

--- maze.js
+++ maze.js
@@ -1,11 +1,6 @@
 window.onload = function () {
-    var gameOn = true;
     var wall1 = document.getElementById("boundary1");
-    var walls = document.getElementsByClassName(".boundary");
-    
-
     wall1.addEventListener("mouseover", loseF);
-    //wall1.addEventListener("mouseout", function (){wall1.style.background = "#eee"});  
 }
 
 function loseF () {
