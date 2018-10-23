c76df3872a851e47f06f7f7dc4fb73f3d6bb4ac2
Lindsay Pink <43824268+LindsayPink@users.noreply.github.com>
2018-10-22 19:55:09 -0500

    comments added for array, loseF and loseA

--- maze.js
+++ maze.js
@@ -2,17 +2,18 @@ window.onload = function () {
     let wall1 = document.getElementById("boundary1");
     wall1.addEventListener("mouseover", loseF);
     aWall = document.querySelectorAll(".boundary");
-    Array.from(aWall).forEach(function(wall) {
+
+    Array.from(aWall).forEach(function(wall) { //runs loseA once one of the five walls is touched
         wall.addEventListener("mouseover", loseA)
     });
 }
 
-function loseF () {
+function loseF () { //loss as a result of touching the first wall
     let wall1 = document.getElementById("boundary1");
     wall1.classList.add("youlose");
 }
 
-function loseA () {
+function loseA () { //loss as a result of touching any walls
     let walls = document.querySelectorAll(".boundary");
     document.querySelector(".boundary").addEventListener("mouseover", loseA);
     for (let i = 0; i < walls.length-1; i++) {
