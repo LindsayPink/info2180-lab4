7444f9bd537b196f7d91d2a1de86d0af5c73384e
Lindsay Pink <43824268+LindsayPink@users.noreply.github.com>
2018-10-22 20:26:09 -0500

    restartable maze

--- maze.js
+++ maze.js
@@ -9,6 +9,9 @@ window.onload = function () {
     
     let end = document.getElementById("end");
     end.addEventListener("mouseover", win);
+
+    let start = document.getElementById("start");
+    start.addEventListener("click", reset); //game restarts on click of S
 }
 
 let touch = false; //keeps track of wall touches during game
@@ -32,5 +35,13 @@ function loseA () { //loss as a result of touching any walls
 function win () {
     if (touch === false) {
         alert("You win!");  
+    }
+}
+
+function reset () { //resets game to initial values so user can try again
+    let walls = document.querySelectorAll(".boundary");
+    document.querySelector(".boundary").addEventListener("mouseover", loseA);
+    for (let i = 0; i < walls.length-1; i++) {
+            walls[i].classList.remove("youlose");
     }
 }
