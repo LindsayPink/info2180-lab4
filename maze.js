2a97e16cb661e72affad1481d3b94c3db60428df
Lindsay Pink <43824268+LindsayPink@users.noreply.github.com>
2018-10-22 19:51:56 -0500

    all boundaries glow red on hover

--- maze.js
+++ maze.js
@@ -1,9 +1,21 @@
 window.onload = function () {
-    var wall1 = document.getElementById("boundary1");
+    let wall1 = document.getElementById("boundary1");
     wall1.addEventListener("mouseover", loseF);
+    aWall = document.querySelectorAll(".boundary");
+    Array.from(aWall).forEach(function(wall) {
+        wall.addEventListener("mouseover", loseA)
+    });
 }
 
 function loseF () {
-    var wall1 = document.getElementById("boundary1");
+    let wall1 = document.getElementById("boundary1");
     wall1.classList.add("youlose");
+}
+
+function loseA () {
+    let walls = document.querySelectorAll(".boundary");
+    document.querySelector(".boundary").addEventListener("mouseover", loseA);
+    for (let i = 0; i < walls.length-1; i++) {
+            walls[i].classList.add("youlose");
+    }
 }
